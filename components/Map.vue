50f268ef981907db505cc0eb6c8c15db6ceddc38
Dominik Deimel <info@dominikdeimel.com>
2020-03-21 22:14:20 +0100

    initial commit

---
+++ components/Map.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      Map goes here
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Map'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <map/>
+  </div>
+</template>
+
+<script>
+  import Map from '~/components/Map'
+
+  export default {
+    components: {
+      Map
+    }
+  }
+</script>
